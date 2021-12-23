e2b54b38191ec6cbca0dba42b9f736004e3a367a
LaupWing <loc_strict@hotmail.com>
2021-12-23 19:17:17 +0100

    searchBar

---
+++ src/components/TopHeader/SearchBar/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div className="max-w-xs">
+        <div className="mt-1 relative p-3 rounded-md">
+            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+                <search-icon class="h-5 w-5 text-gray-500" />
+            </div>
+            <input
+                className="bg-gray-50 block w-full pl-10 sm:text-sm border border-gray-300 rounded-md focus:ring-black focus:border-black"
+                type="text"
+                placeholder="Search"
+            />
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+import {SearchIcon} from '@heroicons/vue/outline'
+
+export default defineComponent({
+    name: 'SearchBar',
+    components:{
+        SearchIcon
+    }
+});
+</script>
--- src/components/TopHeader/index.vue
+++ src/components/TopHeader/index.vue
@@ -1,17 +1,19 @@
 <template>
     <header class="flex">
         <logo/>        
-        
+        <search-bar/>
     </header>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import Logo from './Logo/index.vue'
+import SearchBar from './SearchBar/index.vue'
 
 export default defineComponent({
     components:{
-        Logo
+        Logo,
+        SearchBar
     },
     setup() {
         
