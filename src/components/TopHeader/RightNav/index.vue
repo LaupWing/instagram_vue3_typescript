e4781e616b314334c3b55670d1394dbd7284ac72
LaupWing <loc_strict@hotmail.com>
2021-12-24 12:28:10 +0100

    rightnav

---
+++ src/components/TopHeader/RightNav/index.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="flex items-center justify-end space-x-4">
+        <home-icon class="navBtn" />
+        <menu-icon class="h-6 md:hidden cursor-pointer" />
+        <div class="relative navBtn">
+            <paper-airplane-icon class="navBtn rotate-45" />
+            <div
+                class="absolute -top-1 -right-2 text-xs w-5 h-5 bg-red-500 text-white rounded-full flex items-center justify-center animate-pulse"
+            >
+                3
+            </div>
+        </div>
+        <plus-circle-icon/>
+        <user-group-icon class="navBtn" />
+        <heart-icon class="navBtn" />
+        <!-- <img
+            src="{session.user?.image}"
+            alt="profile picture"
+            class="h-10 w-10 object-fill rounded-full"
+        /> -->
+        <button>Sign in</button>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+import {
+    SearchIcon,
+    PlusCircleIcon,
+    UserGroupIcon,
+    HeartIcon,
+    PaperAirplaneIcon,
+    MenuIcon
+} from '@heroicons/vue/outline'
+import {
+    HomeIcon
+} from '@heroicons/vue/solid'
+
+export default defineComponent({
+    components:{
+        SearchIcon,
+        PlusCircleIcon,
+        UserGroupIcon,
+        HeartIcon,
+        PaperAirplaneIcon,
+        MenuIcon,
+        HomeIcon
+    },
+    name: 'RightNav'
+});
+</script>
--- src/components/TopHeader/index.vue
+++ src/components/TopHeader/index.vue
@@ -3,6 +3,7 @@
         <div class="flex justify-between max-w-6xl mx-5 lg:mx-auto">
             <logo/>        
             <search-bar/>
+            <right-nav/>
         </div>
     </header>
 </template>
@@ -11,11 +12,13 @@
 import { defineComponent } from 'vue'
 import Logo from './Logo/index.vue'
 import SearchBar from './SearchBar/index.vue'
+import RightNav from './RightNav/index.vue'
 
 export default defineComponent({
     components:{
         Logo,
-        SearchBar
+        SearchBar,
+        RightNav
     },
     setup() {
         
