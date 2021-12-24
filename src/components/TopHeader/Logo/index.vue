c31dd7f06ed194c7a11680a523ec5c5fcb4becf1
LaupWing <loc_strict@hotmail.com>
2021-12-24 15:59:24 +0100

    logo fix

--- src/components/TopHeader/Logo/index.vue
+++ src/components/TopHeader/Logo/index.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="relative hidden lg:inline-grid w-24 cursor-pointer">
+    <div class="relative hidden lg:inline-grid w-24 cursor-pointer my-auto">
         <img src="@/assets/images/instagram_logo_large.png" alt="logo" />
     </div>
-    <div class="relative lg:hidden w-10 flex-shrink-0 cursor-pointer">
+    <div class="relative lg:hidden w-10 flex-shrink-0 cursor-pointer my-auto">
         <img src="@/assets/images/instagram_logo_small.png" alt="logo" />
     </div>
 </template>
