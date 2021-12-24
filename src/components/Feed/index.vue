cd17a242b422e0245582de34335155fcd178cc06
LaupWing <loc_strict@hotmail.com>
2021-12-24 16:10:06 +0100

    base feed

--- src/components/Feed/index.vue
+++ src/components/Feed/index.vue
@@ -1,10 +1,28 @@
 <template>
-   
+   <main
+      class="
+         grid grid-cols-1
+         md:grid-cols-2 md:max-w-3xl
+         xl:grid-cols-3 xl:max-w-6xl
+         mx-auto
+      "
+   >
+      <section class="col-span-2">
+         <!-- <Stories/>
+            <Posts/> -->
+      </section>
+      <section class="hidden xl:inline-grid md:col-span-1">
+         <div class="fixed top-20">
+            <!-- <MiniProfile/>
+                  <Suggestions/> -->
+         </div>
+      </section>
+   </main>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-   name: 'Feed'
-})
+   name: "Feed",
+});
 </script>
