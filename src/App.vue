a748ae48302db4d7e680e2033baa0133c38219a8
LaupWing <loc_strict@hotmail.com>
2021-12-25 16:09:31 +0100

    added

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
-    <div class="h-screen overflow-y-scroll scrollbar-hide">
-        <top-header />
-        <!-- <div id="nav" class="bg-red-500">
+   <div class="h-screen overflow-y-scroll scrollbar-hide">
+      <top-header />
+      <!-- <div id="nav" class="bg-red-500">
             <router-link to="/">Home</router-link> |
             <router-link to="/about">About</router-link>
         </div>
         <router-view /> -->
-    </div>
+   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import TopHeader from "@/components/TopHeader/index.vue";
 
 export default defineComponent({
-    components: {
-        TopHeader,
-    },
-    setup() {},
+   components: {
+      TopHeader,
+   },
+   setup() {},
 });
 </script>
