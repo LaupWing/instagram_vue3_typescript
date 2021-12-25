080066f46e4de69690cc2557f3795922c4eecacc
LaupWing <loc_strict@hotmail.com>
2021-12-25 16:00:00 +0100

    update

--- src/components/Feed/Posts/Post/index.vue
+++ src/components/Feed/Posts/Post/index.vue
@@ -1,53 +1,51 @@
 <template>
-   <div className='bg-white my-7 border rounded-sm'>
-         <div className='flex items-center p-5'>
+   <div class='bg-white my-7 border rounded-sm'>
+         <div class='flex items-center p-5'>
             <img 
-               src={userImg} 
-               className='rounded-full h-12 w-12 border p-1 mr-3 object-contain' 
+               src="https://autototaalbv.nl/wp-content/uploads/2017/04/profile-placeholder.png" 
+               class='rounded-full h-12 w-12 border p-1 mr-3 object-contain' 
                alt="" 
             />
-            <p className='flex-1 font-bold'>LaupWing</p>
-            <DotsHorizontalIcon className='h-5' />
+            <p class='flex-1 font-bold'>LaupWing</p>
+            <DotsHorizontalIcon class='h-5' />
          </div>
          <img
-            src={img}
+            src="https://autototaalbv.nl/wp-content/uploads/2017/04/profile-placeholder.png"
             alt="image"
-            className='object-cover w-full'
+            class='object-cover w-full'
          />
-         <div className='flex justify-between px-4 pt-4'>
-            <div className='flex space-x-4'>
-               <HeartIcon className='btn' />
-               <ChatIcon className='btn' />
-               <PaperAirplaneIcon className='btn' />
+         <div class='flex justify-between px-4 pt-4'>
+            <div class='flex space-x-4'>
+               <HeartIcon class='btn' />
+               <ChatIcon class='btn' />
+               <PaperAirplaneIcon class='btn' />
             </div>
-            <BookmarkIcon className='btn' />
+            <BookmarkIcon class='btn' />
          </div>
-         <p className='p-5 truncate'>
-            <span className='font-bold mr-1'>{username} </span> {caption}
+         <p class='p-5 truncate'>
+            <span class='font-bold mr-1'>Laupwing </span> caption
          </p>
-         <div className='ml-10 h-20 overflow-y-scroll scrollbar-track-black scrollbar-thin'>
-            {comments.map(comment => (
-               <div key={comment.id} className='flex items-center space-x-2 mb-3'>
-                  <img className='h-7 rounded-full' src={comment.data().userImage} alt="user image" />
-                  <p className='text-sm flex-1'> <span className='font-bold'>{comment.data().username}</span> {comment.data().comment}</p>
-                  <Moment fromNow className='pr-5 text-xs'>
+         <div class='ml-10 h-20 overflow-y-scroll scrollbar-track-black scrollbar-thin'>
+            <!-- {comments.map(comment => (
+               <div key={comment.id} class='flex items-center space-x-2 mb-3'>
+                  <img class='h-7 rounded-full' src={comment.data().userImage} alt="user image" />
+                  <p class='text-sm flex-1'> <span class='font-bold'>{comment.data().username}</span> {comment.data().comment}</p>
+                  <Moment fromNow class='pr-5 text-xs'>
                      {comment.data().timestamp.toDate()}
                   </Moment>
                </div>
-            ))}
+            ))} -->
          </div>
 
-         <form className='flex items-center p-4'>
-            <EmojiHappyIcon className='h-7' />
+         <form class='flex items-center p-4'>
+            <EmojiHappyIcon class='h-7' />
             <input
                placeholder='Add a comment...'
-               className='border-none flex-1 focus:ring-0'
+               class='border-none flex-1 focus:ring-0'
             />
             <button
                type='submit'
-               disabled={!comment.trim()}
-               className='font-semibold text-blue-400'
-               onClick={sendComment}
+               class='font-semibold text-blue-400'
             >
                Post
             </button>
