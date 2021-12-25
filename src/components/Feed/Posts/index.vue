34c64918958ddc537caa91c597a0ddd87e5c9e61
LaupWing <loc_strict@hotmail.com>
2021-12-25 15:47:33 +0100

    Posts

--- src/components/Feed/Posts/index.vue
+++ src/components/Feed/Posts/index.vue
@@ -1,10 +1,16 @@
 <template>
-   
+   <div>
+
+   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Post from './Post/index.vue'
 
 export default defineComponent({
-   name: 'Posts'
+   name: 'Posts',
+   components:{
+      Post
+   }
 })
 </script>
