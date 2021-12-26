5be1cc937ed10a0231c7a9360a0d504e534d5834
LaupWing <loc_strict@hotmail.com>
2021-12-26 15:35:22 +0100

    update

--- src/components/Feed/Stories/index.vue
+++ src/components/Feed/Stories/index.vue
@@ -1,26 +1,32 @@
 <template>
    <div className='flex space-x-2 p-6 mt-8 border-gray-200 border rounded-sm overflow-x-scroll bg-white scrollbar-thin scrollbar-thumb-black'>
-      <Story 
-         img=""
-         username="LaupWing"
+      <story 
+         :img="'https://autototaalbv.nl/wp-content/uploads/2017/04/profile-placeholder.png'"
+         :username="'LaupWing'"
       />
-      <Story 
+      <!-- <story
          img={profile.avatar}
          username={profile.username}
-      />
+      /> -->
    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
 import faker from '@/helpers'
+import {FakerObject} from '@/types/interfaces'
+import Story from './Story/index.vue'
 
 export default defineComponent({
+   components:{
+      Story
+   },
    async setup() {
-      const test = async ()=>{
-         const stories = await faker(20)
+      const init = async ()=>{
+         const stories:FakerObject[] = await faker(20)
+         // console.log()
       }
-      test()
+      init()
       // const getStores = await faker(20)
       // const stories = ref([])
       
